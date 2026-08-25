73c2304bf2cf284b2704ee1ddf8ceb59bd7d6196
FADECK18 <fegaribayf@unac.edu.pe>
2026-08-24 20:07:21 -0500

    Update sidebar title and remove student info

--- app.py
+++ app.py
@@ -2,15 +2,11 @@ import streamlit as st
 import numpy as np
 
 st.title("Proyecto Python for Analytics")
-st.sidebar.title("Parametros")
+st.sidebar.title("Modulo")
 st.write("Elaborado por: Farid Estefano Garibay Fabian")
 
 st.image("foto de python.jpg", width = 450)
 st.sidebar.image("IMAGEN DMC.png", width = 100)
-
-NOMBRE_ESTUDIANTE = "Farid Estefano Garibay Fabian"
-MODULO = "Python for Analytics"
-AÑO = "2026"
 
 modulos = st.sidebar.selectbox ("Selecione un módulo", ["Módulo Listas", "Módulo Arreglos", "Módulo Funciones"])
 if modulos == "Módulo Listas":
