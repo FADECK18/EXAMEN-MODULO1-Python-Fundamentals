fa5bab1db8ce251540c2dbbf6d61654f2e4f4e5d
FADECK18 <fegaribayf@unac.edu.pe>
2026-08-24 19:56:33 -0500

    Remove width specifications for images

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
+import numpy as np
 
 st.title("Proyecto Python for Analytics")
 st.sidebar.title("Parametros")
 st.write("Elaborado por: Farid Estefano Garibay Fabian")
 
-st.image("foto de python.png", width = 200)
-st.sidebar.image("IMAGEN DMC.png", width = 100)
+st.image("foto de python.png")
+st.sidebar.image("IMAGEN DMC.png")
