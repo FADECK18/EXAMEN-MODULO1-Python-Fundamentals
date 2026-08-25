7a0ce22eea291664784c4deadd914f6a98ca9a1f
FADECK18 <fegaribayf@unac.edu.pe>
2026-08-24 20:44:54 -0500

    Update student name display format

--- app.py
+++ app.py
@@ -12,8 +12,7 @@ if modulos == "Home":
     st.title("Proyecto Python for Analytics")
     st.image("foto de python.jpg", width=450)
 
-    st.write("# Nombre completo del estudiante")
-    st.write("Farid Estefano Garibay Fabian")
+    st.write("Nombre completo del estudiante : Farid Estefano Garibay Fabian")
 
     st.write("### Nombre del módulo")
     st.write("Python Fundamentals")
