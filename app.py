1502db33f16615ad2bbeb4d205607e09378f7bf2
FADECK18 <fegaribayf@unac.edu.pe>
2026-08-24 20:34:39 -0500

    Enhance app.py with project and student details
    Added project details and student information to the Streamlit app.

--- app.py
+++ app.py
@@ -5,12 +5,23 @@ st.title("Proyecto Python for Analytics")
 st.sidebar.title("Módulo")
 st.write("Elaborado por: Farid Estefano Garibay Fabian")
 
-st.image("foto de python.jpg", width = 450)
 st.sidebar.image("IMAGEN DMC.png", width = 100)
 
 modulos = st.sidebar.selectbox ("Selecione un módulo", ["Home", "Ejercicio 1", "Ejercicio 2", "Ejercicio 3", "Ejercicio 4"])
 if modulos == "Home":
   st.write("Bienvenido al módulo Home")
+
+st.image("foto de python.jpg", width = 450)
+st.title("PROYECTO 1 - APLICACIÓN EN STREAMLIT")
+
+st.write("Nombre completo del estudiante": Farid Estefano Garibay Fabian")
+st.write("Nombre del módulo: Python Fundamentals")
+st.write("Información general del estudiante: Soy contador y busco especializarme en análisis de datos para combinar mi experiencia financiera con nuevas herramientas tecnológicas y así potenciar mi perfil profesional")
+st.write("Año : 2026")
+st.write("Breve descripción del proyecto : El proyecto facilita la aplicación práctica de lo aprendido en clase, consolidando los contenidos del primer módulo de Python Fundamentals")
+
+
+
 
 elif modulos == "Ejercicio 1":
   st.write("Bienvenido al Ejercicio 1")
