896d0aea3ef42d044a6d647182283f61f0a7e435
FADECK18 <fegaribayf@unac.edu.pe>
2026-08-24 20:06:38 -0500

    Implement module selection and student info display
    Added student information and module selection logic.

--- app.py
+++ app.py
@@ -8,3 +8,16 @@ st.write("Elaborado por: Farid Estefano Garibay Fabian")
 st.image("foto de python.jpg", width = 450)
 st.sidebar.image("IMAGEN DMC.png", width = 100)
 
+NOMBRE_ESTUDIANTE = "Farid Estefano Garibay Fabian"
+MODULO = "Python for Analytics"
+AÑO = "2026"
+
+modulos = st.sidebar.selectbox ("Selecione un módulo", ["Módulo Listas", "Módulo Arreglos", "Módulo Funciones"])
+if modulos == "Módulo Listas":
+  st.write("Bienvenido al módulo Listas")
+
+elif modulos == "Módulo Arreglos":
+  st.write("Bienvenido al módulo de Arreglos")
+
+else:
+  st.write("Bienvenido al módulo de Funciones")
