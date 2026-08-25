eccf27de7eadb87e4e982e50d2b82e634069d5d2
FADECK18 <fegaribayf@unac.edu.pe>
2026-08-24 19:55:30 -0500

    Add images to main and sidebar in app.py

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ import streamlit as st
 st.title("Proyecto Python for Analytics")
 st.sidebar.title("Parametros")
 st.write("Elaborado por: Farid Estefano Garibay Fabian")
+
+st.image("foto de python.png", width = 200)
+st.sidebar.image("IMAGEN DMC.png", width = 100)
