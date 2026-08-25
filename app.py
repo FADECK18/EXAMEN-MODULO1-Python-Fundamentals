79ea3a1ab2b529e6b47190b253c90af9825f7d52
FADECK18 <fegaribayf@unac.edu.pe>
2026-08-24 20:12:04 -0500

    Update module selection and welcome messages

--- app.py
+++ app.py
@@ -8,12 +8,18 @@ st.write("Elaborado por: Farid Estefano Garibay Fabian")
 st.image("foto de python.jpg", width = 450)
 st.sidebar.image("IMAGEN DMC.png", width = 100)
 
-modulos = st.sidebar.selectbox ("Selecione un módulo", ["Módulo Listas", "Módulo Arreglos", "Módulo Funciones"])
-if modulos == "Módulo Listas":
-  st.write("Bienvenido al módulo Listas")
+modulos = st.sidebar.selectbox ("Selecione un módulo", ["Home", "Ejercicio 1", "Ejercicio 2", "Ejercicio 3", "Ejercicio 4"])
+if modulos == "Home":
+  st.write("Bienvenido al módulo Home")
 
-elif modulos == "Módulo Arreglos":
-  st.write("Bienvenido al módulo de Arreglos")
+elif modulos == "Ejercicio 1":
+  st.write("Bienvenido al Ejercicio 1")
 
+elif modulos == "Ejercicio 2":
+  st.write("Bienvenido al Ejercicio 2")
+
+elif modulos == "Ejercicio 3":
+  st.write("Bienvenido al Ejercicio 3")
+  
 else:
-  st.write("Bienvenido al módulo de Funciones")
+  st.write("Bienvenido al Ejercicio 4")
