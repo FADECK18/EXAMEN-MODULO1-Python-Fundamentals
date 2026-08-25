322b86f80591e2b0fe40c000a12315c52b020e37
FADECK18 <fegaribayf@unac.edu.pe>
2026-08-24 20:02:28 -0500

    Adjust image sizes in app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ st.title("Proyecto Python for Analytics")
 st.sidebar.title("Parametros")
 st.write("Elaborado por: Farid Estefano Garibay Fabian")
 
-st.image("foto de python.jpg", width = 500)
-st.sidebar.image("IMAGEN DMC.png", width = 150)
+st.image("foto de python.jpg", width = 400)
+st.sidebar.image("IMAGEN DMC.png", width = 100)
 
