8ae8cc0bcc85ffcf7b9437347f24bc4131cc3a57
FADECK18 <fegaribayf@unac.edu.pe>
2026-08-24 20:59:11 -0500

    Increase image width in Home module

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ modulos = st.sidebar.selectbox(
 if modulos == "Home":
 
     st.title("PROYECTO PYTHON FOR ANALYTICS")
-    st.image("foto de python.jpg", width=300)
+    st.image("foto de python.jpg", width=500)
 
     st.write("### Nombre completo del estudiante")
     st.write("Farid Estefano Garibay Fabian")
